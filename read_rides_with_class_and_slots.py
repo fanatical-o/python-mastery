f7f9a3146c7197d22ca5c5c4be64be2f9364bea2
Ryan Pate <fanatical@ufo.exchange>
2023-08-08 14:20:26 -0400

    Using the __slots__ attribute with a Row class.

---
+++ read_rides_with_class_and_slots.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import csv
+
+class Row:
+    __slots__ = ['route', 'date', 'daytype', 'rides']
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides    
+
+def read_rides_with_class(filename):
+    records = []
+
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)
+        for row in rows:
+            a_row = Row(route=row[0],
+            date=row[1],
+            daytype=row[2],
+            rides=int(row[3]))
+            record = (a_row)
+            records.append(record)
+    return records
+
+
+if __name__ == '__main__':
+    import tracemalloc
+    tracemalloc.start()
+    rows = read_rides_with_class('Data/ctabus.csv')
+    print('Memory Use: Current %d, Peak %d' % tracemalloc.get_traced_memory())
